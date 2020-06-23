0e8f5700273b9fa8af78b0e56d226853cb7146d1
Ksieciu <antelius14@gmail.com>
2020-06-23 23:29:12 +0200

    Added some comments

--- movieshop/app/Http/Controllers/AdminPanelController.php
+++ movieshop/app/Http/Controllers/AdminPanelController.php
@@ -10,14 +10,14 @@ use App\Categories;
 
 class AdminPanelController extends Controller
 {
+    // get all movies in database, order them by title and paginate
     function show_all(){
         $movies = Movies::orderBy('title', 'asc')->paginate(20);
 
         return view('admin.adminlistMovies', ['movies'=>$movies]);
     }
 
-    
-
+    // show one movie info, based on given id. return infos about movie, movie category(pivot between movies and category) and all categories
     function show_movie($id){
         $movie = Movies::find($id);
         $movie_categories = $movie->Categories;
@@ -26,6 +26,7 @@ class AdminPanelController extends Controller
         return view('admin.adminEditMovie', ['movie'=>$movie, 'movie_categories'=>$movie_categories, 'categories'=>$categories]);
     }
 
+    // update all info of movie based in sent POST input(with PUT method) and given id
     function update_movie(Request $request, $id){
 
         // if($request->has('update_info')){
@@ -44,7 +45,7 @@ class AdminPanelController extends Controller
         
     }
 
-
+    // get movie_id and category_id and detach pivot table from movie
     function delete_movie_category($movie_id, $category_id){
         $movie = Movies::find($movie_id);
         $movie->categories()->detach($category_id);
@@ -55,6 +56,7 @@ class AdminPanelController extends Controller
         return view('admin.adminEditMovie', ['movie'=>$movie, 'movie_categories'=>$movie_categories, 'categories'=>$categories]);
     }
 
+    // delete movie with given id
     function delete_movie($id){
         $movie = Movies::find($id);
         $movie->delete();
@@ -62,6 +64,7 @@ class AdminPanelController extends Controller
         return redirect()->route('admin.listMovies');
     }
 
+    // find movie by given id, then take category id from request and attach both to create pivot
     function add_category_to_movie(Request $request, $movie_id){
         $movie = Movies::find($movie_id);
         $movie->categories()->attach($request->input('categories'));
