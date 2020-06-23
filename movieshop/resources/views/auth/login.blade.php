217b5f95c293ade4ec4d6f8d7d108567a8c106d2
Piotr Wróblewski <piotrek.wroblewski7@wp.pl>
2020-06-23 22:36:46 +0200

    Register

--- movieshop/resources/views/auth/login.blade.php
+++ movieshop/resources/views/auth/login.blade.php
@@ -9,14 +9,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="login-header">{{ __('Login') }}</div>
+                <div class="login-header">{{ __('Logowanie') }}</div>
 
                 <div class="login-wrapper">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="login-box">
-                            <label for="email" class="label-style">{{ __('E-Mail Address:') }}</label>   <br>   
+                            <label for="email" class="label-style">{{ __('Adres e-mail:') }}</label>   <br>   
                             <input id="email" placeholder="Enter your e-mail address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             <br> @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -24,7 +24,7 @@
                                     </span>
                                 @enderror
 
-                            <label for="password" class="label-style">{{ __('Password:') }}</label><br>
+                            <label for="password" class="label-style">{{ __('Hasło:') }}</label><br>
                                 <input id="password" placeholder="Enter your password"  type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 <br>
                                 @error('password')
@@ -45,12 +45,12 @@
                        
                             
                                 <button type="submit" class="btn-login">
-                                    {{ __('Login') }}
+                                    {{ __('Zaloguj') }}
                                 </button><br><br>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Zapomniałeś hasła?') }}
                                     </a>
                                 @endif
 
--- movieshop/resources/views/auth/register.blade.php
+++ movieshop/resources/views/auth/register.blade.php
@@ -6,52 +6,40 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
+                <div class="login-header">{{ __('Rejestracja') }}</div>
+
+                <div class="login-wrapper">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                        <div class="login-box">
+                            <label for="email" class="label-style">{{ __('Adres e-mail') }}</label>
+                            <br>
+                                <input id="email" type="email" placeholder="Wprowadź adres e-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <br>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <label for="password" class="label-style">{{ __('Hasło') }}</label>
+                            <br>
+                                <input id="password" placeholder="Wprowadź hasło" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <br>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                            <label for="password-confirm" class="label-style">{{ __('Potwiedź hasło') }}</label>
+                            <br>
+                                <input id="password-confirm" placeholder="Potwierdź swoje hasło" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <br><br>
+                                <button type="submit" class="btn-login">
+                                    {{ __('Rejestruj') }}
                                 </button>
                             </div>
                         </div>
