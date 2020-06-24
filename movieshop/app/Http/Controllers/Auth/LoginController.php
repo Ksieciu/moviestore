e46ceda99d31e7edc82bfb0ce3dc39b552356e40
Ksieciu <antelius14@gmail.com>
2020-06-24 20:25:11 +0200

    tries in category validation

--- movieshop/app/Http/Controllers/Auth/LoginController.php
+++ movieshop/app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function logout(Request $request) {
+    public function logout() {
         Auth::logout();
         return redirect('/login');
       }
